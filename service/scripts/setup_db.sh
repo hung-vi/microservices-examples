adc33164225260601e8eb98d6e3c6c41a5ad625d
hung-vi <ngochung.vi90@gmail.com>
2019-05-20 09:37:33 +0700

    user-service Add user table

--- service/scripts/setup_db.sh
+++ service/scripts/setup_db.sh
@@ -2,7 +2,10 @@
 
 set -eu
 
-SQLS_DIR=$(cd ./sqls && pwd)
+cd `dirname $0`
+SQLS_DIR=$(cd $(dirname $0) && cd ./sqls && pwd)
+
+echo $SQLS_DIR
 
 function initialize_db {
     local databaseName=$1
@@ -12,7 +15,7 @@ function initialize_db {
     echo -e "\n -------- \n ${sqlFiles} \n --------"
     for sqlFile in ${sqlFiles[@]}; do
         echo -e "** run sql $sqlFile **"
-        echo `docker-compose run --rm -v $SQLS_DIR:/tmp/sqls postgresql psql -U vnext -d "$databaseName" -h postgresql -f /tmp/sqls/$sqlFile`
+        echo `docker-compose run --rm -v $SQLS_DIR:/tmp/sqls postgresql psql -U vnext "$databaseName" -h postgresql -f /tmp/sqls/$sqlFile`
     done
 }
 
@@ -20,10 +23,9 @@ function initialize_db {
 echo -e "\n -------------------- \nCreate databases \n --------------------"
 echo `docker-compose run --rm postgresql psql -U vnext -h postgresql -f - << EOS
 CREATE USER kong;
-CREATE USER vnext_user;
 
 CREATE DATABASE kong OWNER kong;
-CREATE DATABASE user;
+CREATE DATABASE "user";
 EOS`
 
 user_sqls=(
