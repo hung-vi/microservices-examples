8e2bd650ead45d3fc52b4c71fe5dd62fb6c99c5b
hung-vi <ngochung.vi90@gmail.com>
2019-05-21 17:26:38 +0700

    service Init account-service

---
+++ service/account-service/src/test/java/com/vnext/projekt/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnext.projekt.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
