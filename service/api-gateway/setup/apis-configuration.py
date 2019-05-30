304ad1fde25e0b705cc10aad1865d285e8eea380
hung-vi <ngochung.vi90@gmail.com>
2019-05-30 18:14:58 +0700

    [kong] APIs & services configuration for account-service

---
+++ service/api-gateway/setup/apis-configuration.py
@@ -0,0 +1,74 @@
+#/usr/bin/python
+
+import requests
+import json
+
+kongHost = 'localhost'
+kongPort = '8001'
+
+services = [
+    {
+        'name': 'account-service-sys-users',
+        'url': 'http://account-service:8080/sys/users'
+    }
+]
+
+apis = [
+    {
+        'name' : 'sysApi.users',
+        'paths': ['/sys/v1/users'],
+        'hosts': ['http://account-service:8080/sys/users']
+    }
+]
+
+def getServiceByName(_serviceName):
+    kongApi = 'http://' + kongHost + ':' + kongPort + '/services/' + _serviceName
+    res = requests.get(url=kongApi)
+    if res.status_code == 200:
+        return res.json()
+    return None
+
+
+def addService(_name, _url):
+    service = getServiceByName(_name)
+    if service is not None:
+        return service['id']
+    kongApi = 'http://' + kongHost + ':' + kongPort + '/services/'
+    body = {
+        'name': _name,
+        'url': _url
+    }
+    res = requests.post(url = kongApi, data=body)
+    if res.status_code == 201:
+        print 'Add service [' + _name + '] [' + _url + '] --> OK'
+        return res.json()['id']
+    print 'Add service [' + _name + '] [' + _url + '] --> FAILED'
+    return None
+
+def addApis(_apis):
+    for api in _apis:
+        serviceId = addService(api['name'], api['hosts'][0])
+
+        print 'Created serviceId = ' + serviceId
+
+        kongApi = 'http://' + kongHost + ':' + kongPort + '/routes'
+        data = {
+            'name': api['name'],
+            'paths': api['paths'],
+            'service': {
+                'id': serviceId
+            }
+        }
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        res = requests.post(url=kongApi, data=json.dumps(data), headers=headers)
+        print res.json()
+        if res.status_code == 201:
+            print 'Add api ['+ api['name'] +'] --> OK'
+        else:
+            print'Add api [' + api['name'] + '] --> FAILED'
+            break
+
+addApis(apis)
+print 'DONE'
